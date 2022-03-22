70d03c7f423c0cd69692c4c0c5113bc380696742
Mulumia Baraka Mshindi <barakamulumia@gmail.com>
2022-03-22 12:37:13 +0300

    Getting started

---
+++ D01/app.js
@@ -0,0 +1,55 @@
+/**
+ * Variables
+ * a container for data (identifier)
+ */
+
+//declaring a variable
+var username = "baraka";
+let lastName = "mulumia";
+const PI = 3.141;
+
+//assigning to variables;
+// username = "baraka";
+// lastName = "mulumia";
+
+//use variables;
+console.log(username);
+
+/**
+ * Rules of naming variables
+ * variables  can only contain letters, numbers, underscores or dollar signs
+ * Even though they can contain numbers variables cannot start with numbers
+ * variables are case sensitive
+ * you cannot re-declare a variable;
+ * variables should not have spaces
+ * variables should not be javascript  keywords
+ */
+// let age;
+// let Age;
+
+//kebab case
+let current_user_name;
+//camel case
+let currentUserName;
+
+//DATA TYPES
+/**
+ * Strings - textual content
+ */
+let name = "Ariana";
+
+/**
+ * number -- numerical data
+ */
+let age = 56;
+
+/**
+ * boolean --  something that evaluates to true of false;
+ */
+let isRaining = true;
+let isAuth = false;
+
+/**
+ * declare relevant variables according to the following scenario.
+ * John Juma of age 45, is a new student  doing a software development course. Their high score is 89.87 and his nickname is johnny.
+ */
