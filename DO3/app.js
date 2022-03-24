47bf7f0d8552040aa2cdee957648d2273fa3adb3
Mulumia Baraka Mshindi <barakamulumia@gmail.com>
2022-03-24 18:13:06 +0300

    Day 3 Conditions

---
+++ DO3/app.js
@@ -0,0 +1,86 @@
+// Comparison operators
+let a = 7;
+let b = 6;
+
+//gt
+console.log("---gt---");
+console.log(a > b);
+
+//gte
+console.log("---gte---");
+console.log(a >= b);
+
+//lt
+console.log("---lt---");
+console.log(a < b);
+
+//lte
+console.log("---lte---");
+console.log(a <= b);
+
+//eq
+console.log("---eq---");
+console.log(a == b);
+
+//identical (strict equality)
+console.log("---seq---");
+console.log(a === b);
+
+//conditional statements - you can be able to execute a certain block of code based on a condition (if);
+
+let username = "baraka";
+let password = "123Asd";
+
+let usernameInput = prompt("Enter username");
+
+//username check;
+if (usernameInput == username) {
+	alert("Username correct enter password");
+	let passwordInput = prompt("Enter password");
+
+	if (passwordInput == password) {
+		alert("Welcome ON BOARD");
+	} else {
+		alert("Wrong password Try again");
+	}
+} else {
+	alert("Wrong username please try again");
+}
+let userAge = prompt("What is your age? ");
+
+if (userAge >= 18) {
+	alert("adult");
+} else {
+	alert("child");
+}
+
+//if below 18  - child
+// 18 upto 35 - youth
+// 35 and above - too old
+
+if (userAge < 18) {
+	console.log("child");
+} else if (userAge <= 35) {
+	console.log("youth");
+} else {
+	console.log("adult");
+}
+
+//switch
+let role = prompt("User role");
+
+if (role == "Admin") {
+	console.log(101);
+} else if (role == "Student") {
+	console.log(301);
+} else if (role == "Trainer") {
+	console.log(406);
+} else if (role == "Faculty") {
+	console.log(109);
+} else {
+	console.log(000);
+}
+//Write a javascript program to process loans based on the following scenarios
+//1. an applicant is eligible for a loan if they have a good credit score and high income
+//2. an applicant is eligible for a loan if they have a good credit and do not have a criminal record
+//3. an applicant is eligible for a loan if they have a good credit score or high income
